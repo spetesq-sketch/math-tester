1b36ebdb0f18afe208f33221974030fdfcc23ba0
lixtr <spetesq@google.com>
2026-08-26 17:29:36 +0300

    Typos corrected, variable names improved

--- src/game.rs
+++ src/game.rs
@@ -1,6 +1,7 @@
 // game
 
 use rand::RngExt;
+use rand::rngs::ThreadRng;
 use rand::seq::IndexedRandom;
 use serde::{Deserialize, Serialize};
 use std::time::{Duration, Instant};
@@ -26,24 +27,24 @@ impl Stats {
 #[derive(Debug)]
 pub enum AnswerType {
     Correct,
-    Wrong(f32),
+    Wrong { correct_answer: f32 },
 }
 
 #[derive(Debug)]
-pub struct AnswerThing {
+pub struct AnswerResult {
     pub answer_type: AnswerType,
     pub time: Duration,
 }
 
 #[derive(Debug)]
 pub enum Respond {
-    Answer(AnswerThing),
+    Answer(AnswerResult),
     Finished(Stats),
 }
 
 #[derive(Copy, Clone, Debug)]
 pub enum ExampleType {
-    Addiction,
+    Addition,
     Subtraction,
     Multiplication,
     Division,
@@ -52,7 +53,7 @@ pub enum ExampleType {
 #[derive(Debug, Clone)]
 pub struct Example {
     pub text_example: String,
-    timer: Instant,
+    timer: Option<Instant>,
     pub time: Duration,
     pub example_type: ExampleType,
     pub answer: f32,
@@ -60,15 +61,14 @@ pub struct Example {
 
 #[derive(Copy, Clone, Debug)]
 pub struct Domain {
-    pub from: f32,
-    pub to: f32,
+    pub from: u32,
+    pub to: u32,
 }
 
 #[derive(Debug)]
 pub struct Game {
     pub domain_first: Domain,
     pub domain_second: Domain,
-    pub current: Option<Example>,
     current_index: usize,
     pub amount: usize,
     pub examples: Vec<Example>,
@@ -87,7 +87,6 @@ impl Game {
         let mut game = Game {
             domain_first,
             domain_second,
-            current: None,
             current_index: 0,
             amount,
             examples: Vec::new(),
@@ -95,44 +94,43 @@ impl Game {
             digit_after_point: digit,
         };
         game.generate(example_types);
-        game.set_current();
         game
     }
+
     pub fn check_answer(&mut self, answer: f32) -> Respond {
-        let (answer_type, elapsed_time) = {
-            if let Some(example) = &mut self.current {
-                example.time = example.timer.elapsed();
-                self.stats.time += example.time;
-
-                let type_of_ans = if answer == example.answer {
-                    self.stats.correct += 1;
-                    AnswerType::Correct
-                } else {
-                    self.stats.wrong += 1;
-                    AnswerType::Wrong(example.answer)
-                };
-
-                (type_of_ans, example.time)
+        let (is_correct, correct_answer, elapsed_time) = {
+            if let Some(example) = self.get_current_mut() {
+                if let Some(timer) = example.timer {
+                    example.time = timer.elapsed();
+                    example.timer = None;
+                }
+                (answer == example.answer, example.answer, example.time)
             } else {
                 return Respond::Finished(self.stats);
             }
+        };
+
+        let resulting_type = if is_correct {
+            self.stats.correct += 1;
+            AnswerType::Correct
+        } else {
+            self.stats.wrong += 1;
+            AnswerType::Wrong { correct_answer }
         };
 
         if !self.next() {
             return Respond::Finished(self.stats);
         }
 
-        Respond::Answer(AnswerThing {
-            answer_type,
+        Respond::Answer(AnswerResult {
+            answer_type: resulting_type,
             time: elapsed_time,
         })
     }
-
     fn next(&mut self) -> bool {
         self.current_index += 1;
-        self.set_current();
-        if let Some(example) = &mut self.current {
-            example.timer = Instant::now();
+        if let Some(example) = self.get_current_mut() {
+            example.timer = Some(Instant::now());
             true
         } else {
             false
@@ -141,26 +139,30 @@ impl Game {
 
     fn generate(&mut self, example_types: Vec<ExampleType>) {
         let mut rng = rand::rng();
-        for _example in 0..self.amount {
+        for _ in 0..self.amount {
             if let Some(example_type) = example_types.choose(&mut rng) {
-                self.examples.push(self.generate_example(*example_type))
+                self.examples
+                    .push(self.generate_example(*example_type, &mut rng))
             }
         }
     }
-    fn set_current(&mut self) {
-        self.current = self.examples.get(self.current_index).cloned();
+
+    fn get_current_mut(&mut self) -> Option<&mut Example> {
+        self.examples.get_mut(self.current_index)
     }
 
-    fn generate_example(&self, example_type: ExampleType) -> Example {
+    pub fn get_current(&self) -> Option<&Example> {
+        self.examples.get(self.current_index)
+    }
+
+    fn generate_example(&self, example_type: ExampleType, rng: &mut ThreadRng) -> Example {
         let mut rng = rand::rng();
 
-        let first_num =
-            rng.random_range(self.domain_first.from as u32..self.domain_first.to as u32) as f32;
-        let second_num =
-            rng.random_range(self.domain_second.from as u32..self.domain_second.to as u32) as f32;
+        let first_num = rng.random_range(self.domain_first.from..self.domain_first.to) as f32;
+        let second_num = rng.random_range(self.domain_second.from..self.domain_second.to) as f32;
 
         let operator = match example_type {
-            ExampleType::Addiction => '+',
+            ExampleType::Addition => '+',
             ExampleType::Subtraction => '-',
             ExampleType::Multiplication => '*',
             ExampleType::Division => '/',
@@ -168,20 +170,21 @@ impl Game {
 
         Example {
             text_example: format!("{} {} {}", first_num, operator, second_num),
-            timer: Instant::now(),
+            timer: None,
             time: Duration::ZERO,
             example_type,
             answer: get_answer(first_num, second_num, example_type, self.digit_after_point),
         }
     }
 }
-fn get_answer(num1: f32, num2: f32, operator: ExampleType, digit: usize) -> f32 {
+
+fn get_answer(first_num: f32, second_num: f32, operator: ExampleType, digit: usize) -> f32 {
     match operator {
-        ExampleType::Addiction => num1 + num2,
-        ExampleType::Subtraction => num1 - num2,
-        ExampleType::Multiplication => num1 * num2,
+        ExampleType::Addition => first_num + second_num,
+        ExampleType::Subtraction => first_num - second_num,
+        ExampleType::Multiplication => first_num * second_num,
         ExampleType::Division => {
-            let result = num1 / num2;
+            let result = first_num / second_num;
             let multiplier = 10f32.powi(digit as i32);
             let rounded = (result * multiplier).round() / multiplier;
             rounded
--- src/main.rs
+++ src/main.rs
@@ -1,39 +1,34 @@
 // main
-
 mod game;
 mod input_helper;
 mod stats;
 
 use crate::game::{Domain, ExampleType};
-use crossterm::style::Stylize;
+use clap::builder::StyledStr;
+use crossterm::style::{StyledContent, Stylize};
 use game::AnswerType;
 use game::Game;
 
 fn main() {
     let mut game = set_start_params();
     let mut history = Vec::new();
-
-    while let Some(current_example) = &game.current {
-        print!("History: ");
-        for symbol in &history {
-            print!("{}", symbol);
-        }
-        println!();
+    while let Some(current_example) = game.get_current() {
+        print_history(&history);
         let user_answer: f32 = input_helper::input(&format!(
             "Enter answer  {} : ",
             current_example.text_example
         ));
-
         let result = game.check_answer(user_answer);
         match result {
-            game::Respond::Answer(answer_type) => {
-                println!("Answer status: {:?}", answer_type);
-                match answer_type.answer_type {
+            game::Respond::Answer(answer_result) => {
+                println!("Answer status: {:?}", answer_result);
+                match answer_result.answer_type {
                     AnswerType::Correct => history.push('█'.green()),
-                    AnswerType::Wrong(_) => history.push('█'.red()),
+                    AnswerType::Wrong { .. } => history.push('█'.red()),
                 }
             }
             game::Respond::Finished(stats) => {
+                print_history(&history);
                 println!("Finished!\n{:#?}", stats);
                 break;
             }
@@ -42,43 +37,58 @@ fn main() {
 }
 
 fn set_start_params() -> Game {
-    let domain1: String =
+    let first_domain_input: String =
         input_helper::input(r#"Enter first domain like "first second" like "0 100": "#);
-    let domain2: String =
+    let second_domain_input: String =
         input_helper::input(r#"Enter second domain like "first second" like "0 100": "#);
-    let example_types: Vec<usize> = input_helper::input::<String>(
+    let example_type_choices: Vec<usize> = input_helper::input::<String>(
         "Enter types 1 - Addition 2 - Subtraction 3 - Multiplication 4 - Division: ",
     )
     .split_whitespace()
     .map(|s| s.parse().expect("Please enter valid numbers"))
     .collect();
 
-    let mut digit: String =
+    let digit_input: String =
         input_helper::input("Enter number of digit after point (default = 2): ");
-    let mut final_digit = 2usize;
-    if let Ok(final_) = digit.parse::<usize>() {
-        final_digit = final_;
+    let mut precision = 2usize;
+    if let Ok(parsed_precision) = digit_input.parse::<usize>() {
+        precision = parsed_precision;
     }
 
-    let mut a = Vec::new();
-    for i in example_types {
-        let result = match i {
-            1 => ExampleType::Addiction,
+    let mut selected_types = Vec::new();
+    for choice in example_type_choices {
+        let example_type = match choice {
+            1 => ExampleType::Addition,
             2 => ExampleType::Subtraction,
             3 => ExampleType::Multiplication,
             4 => ExampleType::Division,
-            _ => panic!(),
+            _ => panic!("Unknown example type: {choice}"),
         };
-        a.push(result)
+        selected_types.push(example_type)
     }
-    let amount: usize = input_helper::input("Enter amount: ");
-    let firs_domain = get_domain(&domain1);
-    let second_domain = get_domain(&domain2);
 
-    Game::new(firs_domain, second_domain, amount, a, final_digit)
+    let amount: usize = input_helper::input("Enter amount: ");
+    let first_domain = get_domain(&first_domain_input);
+    let second_domain = get_domain(&second_domain_input);
+    Game::new(
+        first_domain,
+        second_domain,
+        amount,
+        selected_types,
+        precision,
+    )
 }
-fn get_domain(str: &str) -> Domain {
-    let parts: Vec<&str> = str.split_whitespace().collect();
+
+fn print_history(history: &Vec<StyledContent<char>>) {
+    print!("History: ");
+    for symbol in history {
+        print!("{}", symbol);
+    }
+    println!();
+}
+
+fn get_domain(input: &str) -> Domain {
+    let parts: Vec<&str> = input.split_whitespace().collect();
     Domain {
         from: parts.first().unwrap().parse().unwrap(),
         to: parts.get(1).unwrap().parse().unwrap(),
